490f75dfc9059d0201455e249159b7f436734683
Chang Mao Yang <jeffrey0613mao@gmail.com>
2025-10-28 18:19:39 +0800

    now can test

--- public/src/main.js
+++ public/src/main.js
@@ -1,4 +1,4 @@
-async function getArticle(id) {
+async function getArticle(id, language) {
     const parser = new DOMParser();
     const url = `https://hackmd.io/@staryuehtech/cms-${id}`;
     const res = await fetch('/api/proxy?url=' + encodeURIComponent(url));
@@ -8,9 +8,17 @@ async function getArticle(id) {
     return htmlDoc.querySelector('#publish-page').innerText;
 }
 
+async function getFakeArticle(id) {
+    const parser = new DOMParser();
+    const res = await fetch('.test.html');
+    if (!res.ok) throw new Error(`${res.status} ${res.statusText}`);
+    return await res.text();
+}
+
 (async () => {
     try {
-        const md = await getArticle('privacy');
+        // const md = await getArticle('privacy', 'zh_TC');
+        const md = await getFakeArticle();
         const html = marked.parse(md);
         console.log(html);
     } catch (err) {
